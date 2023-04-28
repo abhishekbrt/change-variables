add239486109c9661572c76eecb87971cd91a530
Abhishek bharti <abhishekbharti.yz@gmail.com>
2023-04-28 15:12:18 +0530

    added code for blurr and color change

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 console.log("hello javascript");
 
 let space=document.querySelector("#space-slider");
-let abc=document.querySelector(".image-box");
+let imageBox=document.querySelector(".image-box");
+let picture=document.querySelector(".image");
+let blurSlider=document.querySelector("#blur-slider");
+let colorInput=document.querySelector("#base");
+let jsColor=document.querySelector("span");
+jsColor.style.color= "#ffff00";
 
 space.addEventListener("input", function(){
    let bor=space.value;
-   abc.style.borderWidth=`${bor}px`;
-   console.log(bor);
+   imageBox
+.style.borderWidth=`${bor}px`;
+   // console.log(bor);
 });
+
+ blurSlider.addEventListener("input",function(){
+   let value=blurSlider.value;
+   // console.log(value/10);
+   picture.style.filter=`blur(${value/10}px)`;
+ })
+
+ colorInput.addEventListener("input",function(event){
+   const selectedColor=event.target.value;
+   // console.log(selectedColor);
+   imageBox.style.borderColor=selectedColor;
+   jsColor.style.color=selectedColor;
+ })
