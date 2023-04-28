ca7171f3f9067178b8db85641146cbdd60b1e12e
Abhishek bharti <abhishekbharti.yz@gmail.com>
2023-04-28 10:18:44 +0530

    updated space slider

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-console.log('hello world');
+console.log("hello javascript");
 
-let space= document.querySelector("#space-slider");
+let space=document.querySelector("#space-slider");
+let abc=document.querySelector(".image-box");
 
+space.addEventListener("input", function(){
+   let bor=space.value;
+   abc.style.borderWidth=`${bor}px`;
+   console.log(bor);
+});
